a3e51fa5c5c28f38bf14a44ae62d53d811ec0229
gary <gary.white@lodgit.com>
2021-11-22 12:55:28 +0100

    code working

---
+++ code.js
@@ -0,0 +1,8 @@
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height;
+    }
+  }
+
+  console.log(Kata.getVolumeOfCuboid(1,2,2)); // Return 4
+  console.log(Kata.getVolumeOfCuboid(6.3,2,5)); // Return 63
